c6533c2ea5472b3e8ce7ab75a937d492855d703f
Eugen Wintersberger <eugen.wintersberger@desy.de>
2012-03-15 16:25:07 +0100

    Unit test for NXFile is running again. Need to write now tests for all other exposed object.

--- test/NXFileTest.py
+++ test/NXFileTest.py
@@ -1,54 +1,30 @@
 import sys
 import unittest
 
-
-from pni.utils import Array
-from pni.utils import Float32Scalar
-
-
-
-sys.path.insert(0,"../pni")
-
-from nx.h5 import NXFile
+from pni.nx.h5 import NXFile
+from pni.nx.h5 import create_file
+from pni.nx.h5 import open_file
 
 
 #implementing test fixture
 class NXFileTest(unittest.TestCase):
     def setUp(self):
-        self.gf = NXFile()
-        self.gf.overwrite = True
-        self.gf.readonly = False
-        self.gf.filename = "NXFileTest.h5"
-
-        self.gf.create()
+        self.gf = create_file("NXFileTest.h5",overwrite=True)
 
     def tearDown(self):
         self.gf.close()
 
     def test_creation(self):
-        f = NXFile()
-
-        f.filename = "test.h5"
-        f.overwrite = True
-        f.read_only = True
-
-        f.create()
-        self.assertTrue(f.is_open())
+        f = create_file("test.h5",overwrite=True)
+        self.assertTrue(f.valid)
         f.close()
     
-        f.overwrite = False
-        self.assertRaises(UserWarning,f.create)
+        self.assertRaises(UserWarning,create_file,"test.h5",False)
 
     def test_attributes(self):
         s = "a string attribute"
-        self.gf.set_attr("text","a string attribute")
-        g = self.gf.create_group("data")
-        print "createed group", g
+        a = self.gf.attr("text","string")
+        a.value = s
+        self.assertTrue(a.value == s)
 
-        s = Float32Scalar("sca","a.u.","a test scalar")
-        s.value = 100.
-        self.gf.set_attr("sca",s)
-
-        a = Array(shape=(10,5),dtype="uint16")
-        #self.gf.set_attr("det",a)
 
