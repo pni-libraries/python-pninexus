ade9cce1864f3f7a7285b41269b1949e29f3ea5b
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2012-09-25 11:53:59 +0100

    C++ testing main program is compiling.

---
+++ test/pypninxtest.cpp
@@ -0,0 +1,50 @@
+/*
+ * Declaration of Nexus object template.
+ *
+ * (c) Copyright 2011 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of libpninx.
+ *
+ * libpninxpython is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libpninxpython is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libpninxpython.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+#include<cppunit/extensions/HelperMacros.h>
+#include<cppunit/TestCaller.h>
+#include<cppunit/TestResult.h>
+#include<cppunit/TestRunner.h>
+#include<cppunit/TextTestProgressListener.h>
+#include<cppunit/ui/text/TextTestRunner.h>
+#include<cppunit/extensions/TestFactoryRegistry.h>
+
+
+int main(int argc,char **argv){
+
+    //setup the test runner
+    CppUnit::TextTestRunner runner;
+    CppUnit::TextTestProgressListener progress;
+    CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+    
+    runner.addTest(registry.makeTest());
+    runner.eventManager().addListener(&progress);
+    
+    runner.run();
+
+
+    return 0;
+}
+
