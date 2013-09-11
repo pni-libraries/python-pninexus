77d45790805d53ea37e67637b8d03383a0f0a764
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-09-11 14:33:55 +0200

    Fixed Popen problems

--- pkgconfig.py
+++ pkgconfig.py
@@ -1,6 +1,6 @@
 try:
     from subprocess import check_output
-
+    
     def execute(l):
         return check_output(l)
 
@@ -9,7 +9,7 @@ except:
     from subprocess import PIPE
 
     def execute(l):
-        p = Popen(l,shell=True,stdout=PIPE)
+        p = Popen(l,stdout=PIPE)
         result = ""
 
         for x in p.stdout: result+=x
@@ -65,30 +65,25 @@ class package(object):
 
     def _get_library_dirs(self):
         result = execute([self.command,'--libs-only-L',self.name])
-        #result = check_output([self.command,'--libs-only-L',self.name])
         return split_result(result,'-L')
 
     def _get_include_dirs(self):
         result = execute([self.command,'--cflags-only-I',self.name])
-        #result = check_output([self.command,'--cflags-only-I',self.name])
         return split_result(result,'-I')
 
     def _get_libraries(self):
         result = execute([self.command,'--libs-only-l',self.name])
-        #result = check_output([self.command,'--libs-only-l',self.name])
         return split_result(result,'-l')
 
     def _get_compiler_flags(self):
         #first we obtain all compiler flags
         total_result = execute([self.command,'--cflags',self.name])
-        #total_result = check_output([self.command,'--cflags',self.name])
         total_result = total_result.strip()
         total_result = total_result.split(" ")
         total_result = remove_empty_strings(total_result)
 
         #now we have to obtain all the include files
         includes = execute([self.command,'--cflags-only-I',self.name])
-        #includes = check_output([self.command,'--cflags-only-I',self.name])
         includes = includes.strip()
         includes = includes.split(" ")
         includes = remove_empty_strings(includes)
@@ -103,4 +98,20 @@ class package(object):
     libraries    = property(_get_libraries)
     compiler_flags = property(_get_compiler_flags)
     include_dirs = property(_get_include_dirs)
+
+#testing routine
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print "You have to pass a package name to as a command line argument!"
+        sys.exit()
+
+    name = sys.argv[1]
+
+    p = package(name)
+    print "library directories: ",p.library_dirs
+    print "libraries          : ",p.libraries
+    print "compiler flags     : ",p.compiler_flags
+    print "include directories: ",p.include_dirs
 
