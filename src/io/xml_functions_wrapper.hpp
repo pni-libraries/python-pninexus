a080dc0273f40f90f3c3dcd4c489a37c521e036d
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2015-09-16 17:12:20 +0200

    First attempt to implement the xml functionality
    This causes an internal compiler error when translating the code
    on Debian Wheezy. Need to check this on Jessie.

---
+++ src/io/xml_functions_wrapper.hpp
@@ -0,0 +1,58 @@
+//
+// (c) Copyright 2011 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+// This file is part of python-pniio.
+//
+// python-pniio is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// python-pniio is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with pyton-pniio.  If not, see <http://www.gnu.org/licenses/>.
+// ===========================================================================
+//
+// Created on: Feb 17, 2012
+//     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+
+#pragma once
+
+#include <pni/core/utilities.hpp>
+#include <pni/io/nx/xml.hpp>
+
+template<typename GTYPE> struct xml_functions_wrapper
+{
+    typedef GTYPE group_type; 
+
+    static void xml_to_nexus(const pni::core::string &xml_data,
+                             const group_type &parent)
+    {
+        using namespace pni::io::nx;
+        xml::nexus_to_xml(xml::create_from_string(xml_data),parent);
+    }
+    
+};
+
+
+//!
+//! \ingroup wrappers
+//! \brief create NXGroup wrapper
+//! 
+//! Template function to create a new wrapper for an NXGroup type GType.
+//! \param class_name name for the Python class
+//!
+template<typename GTYPE> void create_xml_function_wrappers()
+{
+    typedef xml_functions_wrapper<GTYPE> wrapper_type;
+
+    def("xml_to_nexus",&wrapper_type::xml_to_nexus);
+
+#pragma GCC diagnostic pop
+}
+
---
+++ test/nxgroup_rec_iter_test.py
@@ -0,0 +1,71 @@
+import unittest
+import numpy
+
+from pni.io.nx.h5 import nxfile
+from pni.io.nx.h5 import nxgroup
+from pni.io.nx.h5 import create_file
+from pni.io.nx.h5 import open_file
+from pni.core import ShapeMismatchError
+
+from .attributes_test import attributes_test
+
+def write_attribute(a,v):
+    a.value = v
+    return a
+
+#implementing test fixture
+class nxgroup_test(unittest.TestCase):
+    attr_tester = attributes_test()
+
+    def setUp(self):
+        self.gf = create_file("test/nxgroup_rec_iter_test.nxs",overwrite=True)
+        self.root = self.gf.root()
+        entry = self.root.create_group("entry",nxclass="NXentry")
+        entry.create_group("instrument",nxclass="NXinstrument")
+        entry.create_group("control",nxclass="NXmonitor")
+
+    def tearDown(self):
+        self.root.close()
+        self.gf.close()
+
+    def test_creation(self):
+        g = self.root.create_group("metadata")
+        self.assertTrue(g.is_valid)
+        g = self.root.create_group("scan_1",nxclass="NXentry")
+        self.assertTrue(g.attributes["NX_class"].value == "NXentry")
+
+        g = g.create_group("instrument","NXinstrument").\
+              create_group("detector","NXdetector")
+        self.assertTrue(g.is_valid)
+        self.assertTrue(g.name == "detector")
+
+    def test_open(self):
+        #try to open a group that does not exist
+        self.assertRaises(KeyError,self.root.__getitem__,"data")
+
+    def test_simple_attributes(self):
+        g = self.root.create_group("dgroup")
+        self.assertTrue(g.is_valid)
+        self.attr_tester.test_scalar_attribute(self,g)
+
+    def test_array_attributes(self):
+        g = self.root.create_group("dgroup")
+        self.assertTrue(g.is_valid)
+        self.attr_tester.test_array_attribute(self,g)
+
+    def test_group_iteration(self):
+        g = self.root.create_group("scan_1","NXentry").\
+                      create_group("instrument","NXinstrument").\
+                      create_group("detector","NXdetector")
+        g.create_group("module_1")
+        g.create_group("module_2")
+        g.create_group("module_3")
+
+        self.assertTrue(g.size==3)
+
+        i = 1 
+        for m in g:
+            self.assertTrue(m.name == "module_%i" %(i))
+            i += 1
+
+        
