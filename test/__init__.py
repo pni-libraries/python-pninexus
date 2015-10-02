90d2430571af68598b45050b287f640875baf53a
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2015-10-02 12:32:21 +0200

    Start with new module for group tests.

--- test/__init__.py
+++ test/__init__.py
@@ -1,10 +1,10 @@
 from .core         import *
 from .algorithms   import *
 from .nxfile_tests import *
+from .nxgroup_tests import *
 
 from .issue_48_test import Issue_48_Test
 from .issue_53_test import Issue_53_Test
-from .nxgroup_test import nxgroup_test
 from .nxpath_test import nxpath_test
 from .nxpath_match_test import nxpath_match_test
 
---
+++ test/nxgroup_tests/__init__.py
@@ -0,0 +1,1 @@
+from .nxgroup_test import nxgroup_test
