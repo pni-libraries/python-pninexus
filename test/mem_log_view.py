7e3494ad6b1f62ff933e2109e164f4e7c52a3a19
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2015-10-14 10:42:41 +0200

    Cleaned up the mem_log_view code

--- test/mem_log_view.py
+++ test/mem_log_view.py
@@ -6,6 +6,9 @@ import numpy
 import sys
 import argparse
 
+#-----------------------------------------------------------------------------
+# setting up and parse command line arguments
+#-----------------------------------------------------------------------------
 parser = argparse.ArgumentParser(description="Memory consumption analyzer")
 parser.add_argument("-i","--ignore",dest="ignore",
                     default=0,
@@ -17,24 +20,31 @@ parser.add_argument("logfile",action="store",type=str,
                     help="Logfile to read",
                     metavar="LOGFILE")
 
-
-
 args = parser.parse_args()
 
+# if no logfile has been given by the user - abort the program
 if not args.logfile:
     print("You have to pass a logfile as an argument")
     sys.exit(1)
 
-
+#-----------------------------------------------------------------------------
+# load data from the file
+#-----------------------------------------------------------------------------
 data = numpy.loadtxt(args.logfile)
-run = data[args.ignore:-1,0]
+run  = data[args.ignore:-1,0]
 tmem = data[args.ignore:-1,1]
 rmem = data[args.ignore:-1,2]
 
+#-----------------------------------------------------------------------------
+# print some very basic statistics 
+#-----------------------------------------------------------------------------
 print("start memory : ",rmem[0])
 print("stop  memory : ",rmem[-1])
 print("bytes per run: ",(rmem[-1]-rmem[0])/rmem.size)
 
+#-----------------------------------------------------------------------------
+# plot the results
+#-----------------------------------------------------------------------------
 plt.figure()
 plt.plot(run,rmem)
 plt.title("{logfile}\n resident memory consumption".format(logfile=args.logfile))
