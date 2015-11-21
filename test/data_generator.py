e2522e1f72d8035a689f652bc5a8178603cb83b3
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2015-11-21 14:02:51 +0100

    Added license header to the test files

--- test/data_generator.py
+++ test/data_generator.py
@@ -1,21 +1,21 @@
 #
+# (c) Copyright 2014 DESY, 
+#               2014 Eugen Wintersberger <eugen.wintersberger@desy.de>
 #
-# (c) Copyright 2014 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+# This file is part of python-pni.
 #
-# This file is part of python-pniio.
-#
-# python-pniio is free software: you can redistribute it and/or modify
+# python-pni is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
-# python-pniio is distributed in the hope that it will be useful,
+# python-pni is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with python-pniio.  If not, see <http://www.gnu.org/licenses/>.
+# along with python-pni.  If not, see <http://www.gnu.org/licenses/>.
 # ===========================================================================
 #
 # Created on: May 16, 2014
--- test/nxfield_tests/scalar_io_test.py
+++ test/nxfield_tests/scalar_io_test.py
@@ -1,3 +1,26 @@
+#
+# (c) Copyright 2015 DESY, 
+#               2015 Eugen Wintersberger <eugen.wintersberger@desy.de>
+#
+# This file is part of python-pni.
+#
+# python-pni is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# python-pni is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with python-pni.  If not, see <http://www.gnu.org/licenses/>.
+# ===========================================================================
+#
+# Created on: Oct 7, 2015
+#     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+#
 import unittest
 import numpy
 import os
