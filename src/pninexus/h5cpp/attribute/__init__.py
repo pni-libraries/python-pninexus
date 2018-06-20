e2d1811c1feb819da85a183bfa872d152e6df342
Jan Kotanski <jankotan@gmail.com>
2018-06-20 20:51:04 +0200

    add flake8

--- src/pninexus/h5cpp/attribute/__init__.py
+++ src/pninexus/h5cpp/attribute/__init__.py
@@ -5,37 +5,38 @@ from pninexus.h5cpp import property
 from pninexus.h5cpp._attribute import AttributeManager
 from pninexus.h5cpp._attribute import Attribute
 
+__all__ = [property, AttributeManager, Attribute]
 
-def attribute__getitem__(self,index):
-    
+def attribute__getitem__(self, index):
+
     data = self.read()
-    
+
     return data[index]
-    
-def attribute_write(self,data):
+
+
+def attribute_write(self, data):
 
     write_data = data
-    if not isinstance(write_data,numpy.ndarray):
+    if not isinstance(write_data, numpy.ndarray):
         write_data = numpy.array(write_data)
-        
+
     if write_data.dtype.kind == 'U':
         write_data = write_data.astype("S")
-        
+
     try:
         self._write(write_data)
     except RuntimeError:
-        print(write_data,write_data.dtype)
-        
+        print(write_data, write_data.dtype)
+
+
 def attribute_read(self):
-    
+
     read_data = self._read()
-    
-    if isinstance(read_data,numpy.ndarray) and read_data.dtype.kind=='S':
+
+    if isinstance(read_data, numpy.ndarray) and read_data.dtype.kind == 'S':
         return read_data.astype('U')
     else:
         return read_data
-            
-        
 
 
 Attribute.__getitem__ = attribute__getitem__
--- src/pninexus/h5cpp/filter/__init__.py
+++ src/pninexus/h5cpp/filter/__init__.py
@@ -2,3 +2,5 @@ from pninexus.h5cpp._filter import Filter
 from pninexus.h5cpp._filter import Deflate
 from pninexus.h5cpp._filter import Shuffle
 from pninexus.h5cpp._filter import Fletcher32
+
+__all__ = [Filter, Deflate, Shuffle, Fletcher32]
