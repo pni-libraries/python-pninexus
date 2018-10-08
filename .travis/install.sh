ec8480789e4daa13868a4e385156935205f23085
Jan Kotanski <jankotan@gmail.com>
2018-10-08 20:00:59 +0200

    comment build

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,12 +3,12 @@
 
 if [ $2 = "2" ]; then
     echo "install python-pni"
-    docker exec -it ndts python setup.py -q build
-    #docker exec -it --user root ndts python setup.py -q build_sphinx
+    # docker exec -it ndts python setup.py -q build
+    # docker exec -it --user root ndts python setup.py -q build_sphinx
     docker exec -it --user root ndts python setup.py -q install
 else
     echo "install python3-pni"
-    docker exec -it ndts python3 setup.py -q build
+    # docker exec -it ndts python3 setup.py -q build
     # docker exec -it --user root ndts python3 setup.py -q build_sphinx
     docker exec -it --user root ndts python3 setup.py -q install
 fi
--- .travis/run.sh
+++ .travis/run.sh
@@ -2,12 +2,12 @@
 
 if [ $1 = "2" ]; then
     echo "run python-pni"
-    # docker exec -it --user root ndts python setup.py test
-    docker exec -it ndts python setup.py test
+    docker exec -it --user root ndts python setup.py test
+    # docker exec -it ndts python setup.py test
 else
     echo "run python3-pni"
-    # docker exec -it --user root ndts python3 setup.py test
-    docker exec -it ndts python3 setup.py test
+    docker exec -it --user root ndts python3 setup.py test
+    # docker exec -it ndts python3 setup.py test
 fi
 if [ $? -ne "0" ]
 then
