aa766dcc4349a4b49dca6096c8d62389c16a2549
Jan Kotanski <jankotan@gmail.com>
2018-10-08 18:00:06 +0200

    add travis tests

---
+++ .travis/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+if [ $2 = "2" ]; then
+    echo "install python-pni"
+    docker exec -it --user root ndts python setup.py -q build
+    docker exec -it --user root ndts python setup.py -q build_sphinx
+    docker exec -it --user root ndts python setup.py -q install
+else
+    echo "install python3-pni"
+    docker exec -it --user root ndts python3 setup.py -q build
+    docker exec -it --user root ndts python3 setup.py -q build_sphinx
+    docker exec -it --user root ndts python3 setup.py -q install
+fi
+if [ $? -ne "0" ]
+then
+    exit -1
+fi
---
+++ .travis/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ $1 = "2" ]; then
+    echo "run python-pni"
+    docker exec -it ndts python setup.py test
+else
+    echo "run python3-pni"
+    docker exec -it ndts python3 setup.py test
+fi
+if [ $? -ne "0" ]
+then
+    exit -1
+fi
