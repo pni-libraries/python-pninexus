77a38f04b99e749274beb5e4f55a3b7c712b1b48
Jan Kotanski <jankotan@gmail.com>
2018-10-08 19:14:55 +0200

    add g++

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,13 +3,13 @@
 
 if [ $2 = "2" ]; then
     echo "install python-pni"
-    # docker exec -it --user root ndts python setup.py -q build
-    # docker exec -it --user root ndts python setup.py -q build_sphinx
+    docker exec -it --user root ndts python setup.py -q build
+    docker exec -it --user root ndts python setup.py -q build_sphinx
     docker exec -it --user root ndts python setup.py -q install
 else
     echo "install python3-pni"
-    # docker exec -it --user root ndts python3 setup.py -q build
-    # docker exec -it --user root ndts python3 setup.py -q build_sphinx
+    docker exec -it --user root ndts python3 setup.py -q build
+    docker exec -it --user root ndts python3 setup.py -q build_sphinx
     docker exec -it --user root ndts python3 setup.py -q install
 fi
 if [ $? -ne "0" ]
