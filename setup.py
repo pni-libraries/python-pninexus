c2263d14399acd894eecb45147a60819ec4da7a8
Jan Kotanski <jankotan@gmail.com>
2019-04-03 21:42:47 +0200

    bump the version

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ from build_tools import (CppExtensionFactory,
 
 cmdclass = {'build_sphinx': BuildDoc}
 name = "pninexus"
-version = "1.2.11"
-release = "1.2.11"
+version = "1.2.12"
+release = "1.2.12"
 
 
 def get_build_dir():
