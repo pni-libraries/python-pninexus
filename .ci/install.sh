57e2aed5891b36a2f4031e3a58553d35119ff4b2
Jan Kotanski <jankotan@gmail.com>
2021-01-06 17:46:44 +0100

    change the order of builds

--- .ci/install.sh
+++ .ci/install.sh
@@ -8,21 +8,17 @@ then
 fi
 
 if [ "$1" = "2" ]; then
-    echo "build python-pni"
+    echo "install python-pni"
     docker exec --user root ndts chown -R tango:tango .
-    docker exec --user root ndts python setup.py build
+    docker exec  --user root ndts python setup.py install
     echo "build python3-pni docs"
     docker exec  --user root ndts python setup.py  build_sphinx
-    echo "install python-pni"
-    docker exec  --user root ndts python setup.py install
 else
-    echo "build python3-pni"
+    echo "install python3-pni"
     docker exec  --user root ndts chown -R tango:tango .
-    docker exec  --user root  ndts python3 setup.py build
+    docker exec  --user root ndts python3 setup.py install
     echo "build python3-pni docs"
     docker exec  --user root ndts python3 setup.py  build_sphinx
-    echo "install python3-pni"
-    docker exec  --user root ndts python3 setup.py install
 fi
 if [ $? -ne "0" ]
 then
