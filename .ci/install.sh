e5086d0f406216de9b1c7a5a58426ec844805bd1
Jan Kotanski <jankotan@gmail.com>
2021-01-07 09:27:06 +0100

    remove apt-get from install.sh

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 
-docker exec  --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y libboost-python-dev libboost-dev'
-if [ $? -ne "0" ]
-then
-    exit -1
-fi
-
 if [ "$1" = "2" ]; then
     echo "install python-pni"
     docker exec --user root ndts chown -R tango:tango .
