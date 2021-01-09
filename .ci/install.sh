260ea21267269ce606e82cfb07b36fd772d6fc25
Jan Kotanski <jankotan@gmail.com>
2021-01-09 15:03:53 +0100

    change python-pni to python-pninexus (#121)

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,16 +2,16 @@
 
 
 if [ "$1" = "2" ]; then
-    echo "install python-pni"
+    echo "install python-pninexus"
     docker exec --user root ndts chown -R tango:tango .
     docker exec  --user root ndts python setup.py install
-    echo "build python3-pni docs"
+    echo "build python3-pninexus docs"
     docker exec  --user root ndts python setup.py  build_sphinx
 else
-    echo "install python3-pni"
+    echo "install python3-pninexus"
     docker exec  --user root ndts chown -R tango:tango .
     docker exec  --user root ndts python3 setup.py install
-    echo "build python3-pni docs"
+    echo "build python3-pninexus docs"
     docker exec  --user root ndts python3 setup.py  build_sphinx
 fi
 if [ $? -ne "0" ]
--- src/cpp/h5cpp/_h5cpp.cpp
+++ src/cpp/h5cpp/_h5cpp.cpp
@@ -1,20 +1,20 @@
 //
 // (c) Copyright 2011 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
 //
-// This file is part of python-pniio.
+// This file is part of python-pninexus.
 //
-// python-pniio is free software: you can redistribute it and/or modify
+// python-pninexus is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 2 of the License, or
 // (at your option) any later version.
 //
-// python-pniio is distributed in the hope that it will be useful,
+// python-pninexus is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with python-pniio.  If not, see <http://www.gnu.org/licenses/>.
+// along with python-pninexus.  If not, see <http://www.gnu.org/licenses/>.
 // ===========================================================================
 //
 //  Created on: Jan 5, 2012
