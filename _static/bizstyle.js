0b7a21b3dc89307ac6bdfcaff407aa1885a886d4
Jan Kotanski <jankotan@gmail.com>
2021-01-12 08:06:11 +0100

    update the docu

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("pninexus 1.3.1 documentation");
+        $("li.nav-item-0 a").text("pninexus 1.3.2 documentation");
     }
 });
