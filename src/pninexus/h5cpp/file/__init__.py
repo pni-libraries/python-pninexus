cc60a8c10a3a45d55bc07c6dd8d2de700e3f4ec4
Jan Kotanski <jankotan@gmail.com>
2021-01-12 07:50:40 +0100

    add ImageFlags wrapper and bump the version

--- src/pninexus/h5cpp/file/__init__.py
+++ src/pninexus/h5cpp/file/__init__.py
@@ -1,5 +1,6 @@
 from pninexus.h5cpp._file import File
 from pninexus.h5cpp._file import AccessFlags
+from pninexus.h5cpp._file import ImageFlags
 from pninexus.h5cpp._file import Scope
 
 #
@@ -10,5 +11,5 @@ from pninexus.h5cpp._file import open
 from pninexus.h5cpp._file import is_hdf5_file
 from pninexus.h5cpp._file import from_buffer
 
-__all__ = ["File", "AccessFlags", "Scope", "create", "open", "is_hdf5_file",
-           "from_buffer"]
+__all__ = ["File", "ImageFlags", "AccessFlags", "Scope", "create", "open",
+           "is_hdf5_file", "from_buffer"]
