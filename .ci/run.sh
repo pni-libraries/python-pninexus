f87aae80614b2f3c6ae3fe4ac07d959450c5e167
Your Name <jankotan@gmail.com>
2021-12-02 17:37:54 +0100

    run tests as root

--- .ci/run.sh
+++ .ci/run.sh
@@ -2,11 +2,11 @@
 
 if [ "$1" = "2" ]; then
     echo "run python-pninexus"
-    docker exec ndts python setup.py test
+    docker exec --user root ndts python setup.py test
     # docker exec -it ndts python setup.py test
 else
     echo "run python3-pninexus"
-    docker exec ndts python3 setup.py test
+    docker exec --user root ndts python3 setup.py test
     # docker exec -it ndts python3 setup.py test
 fi
 if [ "$?" != "0" ]; then exit 255; fi
