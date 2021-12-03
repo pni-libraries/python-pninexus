5c49bed1212af332a6c24563eb0c68cf46800454
Your Name <jankotan@gmail.com>
2021-12-03 13:42:53 +0100

    update tests

--- .ci/run.sh
+++ .ci/run.sh
@@ -2,9 +2,9 @@
 
 if [ "$1" = "2" ]; then
     echo "run python-pninexus tests"
-    docker exec ndts python test
+    docker exec ndts python setup.py test
 else
     echo "run python3-pninexus tests"
-    docker exec ndts python3 -m pytest
+    docker exec ndts python3 -m pytest test
 fi
 if [ "$?" != "0" ]; then exit 255; fi
