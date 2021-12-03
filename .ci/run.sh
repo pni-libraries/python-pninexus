cf6b847ed6ca8ee67e347ec613b9c48993cd518d
Your Name <jankotan@gmail.com>
2021-12-03 14:30:34 +0100

    switch py2 tests to pytest

--- .ci/run.sh
+++ .ci/run.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" = "2" ]; then
     echo "run python-pninexus tests"
-    docker exec ndts bash -c "ls -ltr src"
     docker exec --user root ndts python setup.py test
+    docker exec ndts python -m pytest test
 else
     echo "run python3-pninexus tests"
     docker exec ndts python3 -m pytest test
