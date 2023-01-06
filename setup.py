affda7eb085436122420f50cae66bf0ea5697eec
Jan Kotanski <jankotan@gmail.com>
2023-01-06 10:28:00 +0100

    switch back to latest

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ from build_tools import (CppExtensionFactory,
 cmdclass = {'build_sphinx': BuildDoc}
 name = "pninexus"
 version = "3.0.2"
-release = "3.0.2"
-# release = "3.0"
+# release = "3.0.2"
+release = "3.0"
 
 if release.count(".") == 1:
     docs_release = '(latest)'
