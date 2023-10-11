222784476a5517cb3ce6bdec05b18b1d601dce6b
Jan Kotanski <jankotan@gmail.com>
2023-10-11 20:09:47 +0200

    set sidebarwidth in docu

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -113,7 +113,7 @@ pygments_style = 'sphinx'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {"sidebarwidth":500}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
