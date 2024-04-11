abf3596d3830a9b1d160021ed9e5bd5bf56a7426
jkotan <jan.kotanski@desy.de>
2024-04-11 13:43:15 +0000

    Build: (9cd43d1) Build: (d911bdb) Build: (213d8c7) Build: (1df8ffb) Build: (993becc) Build: (f70df26) Build: (398de46) Build: (253d98e) Build: (a715d12) Build: (c66ba49) Build: (e108931) Build: (2fc5b33) Build: (aa364ba) Build: (7f7a68d) Build: (428d477) Build: (e2430ab) Build: (4ff6a01) Build: (203486a) Build: (707413e) Build: (d2eaf45) Build: (6b48402) Build: (4e3346f) Build: (37f80a7) Build: (7915650) Build: (cfaec67) Build: (dcdc67f) Build: (9d14963) Build: (48d3a46) Build: (5954c34) Build: (807d736) Build: (6182797) Build: (91dc1c9) Build: (a068aaf) Build: (7999a15) Build: (3b2009a) Build: (5c2c0e1) Build: (1b288b6) Build: (313e28f) Build: (8750ab8) Build: (01f558d) Build: (a43be7e) Build: (c89d975) Build: (a2f0780) Build: (0becdb0) Build: (b56792c) Build: (e238b03) Build: (ac519bf) Build: (dad5a52) Build: (2a224b7) Build: (cef57de) Build: (b3490d6) Build: (1c82338) Build: (ef2e242) Build: (51dbdcd) Build: (5ca6f05) Build: (5f5aff7) Build: (f541721) Build: (2ac94a6) Build: (3f886b6) Build: (a5ccdf1) Build: (d83b379) Build: (07a6283) Build: (8d95906) Build: (9346a92) Build: (d80a710) Build: (bf0defc) Build: (e50f601) Build: (517693a) Build: (d949b21) Build: (a6f81eb) Build: (e116e3d) Build: (9039706) Build: (8fe499c) Build: (903f301) Build: (7d9ec3c) Build: (629e56b) Build: (0d6f007) Build: (f6d16ff) Build: (607c87f) Build: (a223fb2) Build: (0a26f5f) Build: (c0b9f24) Build: (f435ed5) Build: (0bd6cbe) Build: (f90ef09) Build: (0db9d8c) Build: (0bd1e58) Build: (47382a8) Build: (7d4e6cb) Build: (399ac05) Build: (f65908f) Build: (a10491b) Build: (2c612ea) Build: (6ead500) Build: (0fa7ddb) Build: (97c1356) Build: (971745e) Build: (e8af3fd) Build: (50334b1) Build: (a0eddb4) Build: (ea03036) Build: (29ab18c) Build: (4bcb97f) Build: (65818f9) Build: (13c8fcd) Build: (a88482e) Build: (1a45958) Build: (60aa717) Build: (b7f0ee6) Build: (9bf82f1) Build: (8ff57b0) move docs to latest

--- latest/_static/bizstyle.js
+++ latest/_static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "pninexus (latest) documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "libpninexus Python bindings 1.1.0 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
