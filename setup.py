f0feba9215eecb40b939e6485c5f7abc35ebda62
Jan Kotanski <jankotan@gmail.com>
2024-04-11 15:23:51 +0200

    add python 3.12 label

--- setup.py
+++ setup.py
@@ -278,6 +278,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     test_suite="test",
     test_loader="unittest:TestLoader",
